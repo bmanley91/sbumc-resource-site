c79d837e6fd4b93e5b37c5a667a9ea573f9861f0
bmanley91 <brian.manley91@gmail.com>
2021-01-26 22:55:36 -0500

    Added contribution guidelines.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,4 @@
+# Contribution Guidelines
+If you're comfortable contributing yourself, feel free to open a pull request with changes to the data/resources.js file to add resources.
+
+Otherwise, open a [GitHub issue](https://github.com/bmanley91/sbumc-resource-site/issues/new) with an appropriate title and description for your request, or just email the maintainer of this project [link](mailto:brian.manley91@gmail.com).
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import ResourceTable from '@components/ResourceTable'
-
+import Head from 'next/head';
+import Footer from '@components/Footer';
+import Header from '@components/Header';
+import ResourceTable from '@components/ResourceTable';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>SBUMC Resources</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>SBUMC Resources</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Header title="South Bethlehem UMC Team Resources" />
-        <ResourceTable />
-      </main>
-    </div>
-  )
+            <main>
+                <Header title="South Bethlehem UMC Team Resources" />
+                <ResourceTable />
+            </main>
+            
+            <Footer />
+        </div>
+    );
 }
