3a01fc8c33d7488574a63ba2af845d8598f821c4
bmanley91 <brian.manley91@gmail.com>
2021-01-26 22:25:54 -0500

    Added slack and google drive to resource table.

--- data/resources.js
+++ data/resources.js
@@ -2,4 +2,12 @@ export const resourceData = [{
     name: "Trello",
     link: "https://trello.com/b/AscoXdqn/south-bethlehem-umc-board",
     description: "Board that the team uses to track work."
+}, {
+    name: 'Google Drive',
+    link: 'https://drive.google.com/drive/u/0/folders/1bbpz0_tVdSbgZR85IxFs3ctvgF5bzQzQ',
+    description: 'Google Drive folder containing various resources such as retro notes, reference docs, and attendance sheets.'
+}, {
+    name: 'Slack',
+    link: 'https://southbethlehemumc.slack.com',
+    description: 'The Slack workspace the team uses to communicate.'
 }]
