a953cc38816a70a7bf38f475b286974e4b934978
bmanley91 <brian.manley91@gmail.com>
2021-02-07 18:38:02 -0500

    Added team covenant to resources.

--- data/resources.js
+++ data/resources.js
@@ -1,4 +1,8 @@
 export const resourceData = [{
+    name: 'Team Covenant',
+    link: 'https://docs.google.com/document/d/1WW6Sntp3YbnQSOjNs0mR0lUP1xci57njjzh-AyXabiI/edit#',
+    description: 'A Google Doc outling our teams practices.'
+},{
     name: 'Trello',
     link: 'https://trello.com/b/AscoXdqn/south-bethlehem-umc-board',
     description: 'Board that the team uses to track work.'
